04bfffbbb38eafb44a1b67bb84471dccd3f974ec
team19-c <seyeong924@naver.com>
2024-06-14 21:42:14 +0900

    Update new_motor_1641.ino

--- new_motor_1641.ino
+++ new_motor_1641.ino
@@ -9,8 +9,8 @@ int Dir1Pin_B = 6;      // B 제어신호 1핀 (모터)
 int Dir2Pin_B = 7;      // B 제어신호 2핀 (모터)
 int SpeedPin_B = 11;    // B PWM제어를 위한 핀 (모터)
 
-char command;
-bool motorRunning = false;
+char command;           // 헬멧에서 불러오는 시리얼 데이터를 command로 설정
+bool motorRunning = false;  // 처음에 모터 작동 상태를 !Running으로 설정
 
 void setup() {
   Serial.begin(9600);
@@ -26,7 +26,7 @@ void setup() {
 void loop() {
   if (BTSerial.available()) {
     command = BTSerial.read();
-    if (command == 'G' && !motorRunning) {
+    if (command == 'G' && !motorRunning) {   //헬멧 수신 데이터가 'Go'이고 현재 모터 작동 상태가 !Running이라면
       // 모터 최고 속도
       digitalWrite(Dir1Pin_A, HIGH);         //모터(A)가 시계 방향으로 회전
       digitalWrite(Dir2Pin_A, LOW);
@@ -34,8 +34,9 @@ void loop() {
       digitalWrite(Dir1Pin_B, HIGH);         //모터(B)가 시계 방향으로 회전
       digitalWrite(Dir2Pin_B, LOW);
       analogWrite(SpeedPin_B, 255);          //모터 속도를 최대로 설정
-      motorRunning = true;
-    } else if (command == 'S' && motorRunning) {
+      motorRunning = true;                   //모터 작동 상태를 Running으로 설정
+    } else if (command == 'S' && motorRunning) { //헬멧 수신 데이터가 'Stop'이고 현재 모터 작동 상태가 Running이라면
+      motorRunning = false;                  //모터 작동 상태를 !Running으로 설정
       // 모터 감속
       for (int i = 255; i >= 0; i -= 51) {
         analogWrite(SpeedPin_A, i);          //모터 속도를 점차 줄임
@@ -44,7 +45,6 @@ void loop() {
       }
       analogWrite(SpeedPin_A, 0);            // 모터 완전히 멈춤
       analogWrite(SpeedPin_B, 0);            // 모터 완전히 멈춤
-      motorRunning = false;
     }
   }
 }
